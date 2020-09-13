7692d3cfd6d9db3e0b2062c0bc7448c4b2796edf
Max-Home-Tower <max.murphy11@gmail.com>
2020-09-13 10:54:16 -0500

    Add Contents

---
+++ Contents.m
@@ -0,0 +1,6 @@
+% PHASE-PORTRAIT-EXAMPLE
+%
+% Files
+%   initializeGrid    - Initialize grid for phase portrait
+%   main              - Contains code to organize the rest of the example repo
+%   undamped_pendulum - System of equations for ODEs in common undamped pendulum example with no driving force
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-Phase-Portrait-Example
+# Example Undamped Pendulum simulation
+
+Code in this small repository relates to Chapter 3 of the Murphy dissertation. 
+
+Clicking "Run" with `main.m` open in the Matlab editor should generate figures similar to the following:
+
+ 
